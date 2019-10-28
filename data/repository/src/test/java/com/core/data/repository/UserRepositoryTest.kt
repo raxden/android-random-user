152a92650dbade7e6646f1ed343a25abb49fbe64
Ángel <raxden.dev@gmail.com>
2019-10-28 22:00:38 +0100

    add user repository test

--- data/repository/src/test/java/com/core/data/repository/UserRepositoryTest.kt
+++ data/repository/src/test/java/com/core/data/repository/UserRepositoryTest.kt
@@ -1,16 +1,32 @@
 package com.core.data.repository
 
-import com.core.data.BaseRepositoryTest
+import com.core.common.test.BaseTest
+import com.core.data.local.dao.ExcludedUserDao
+import com.core.data.local.model.ExcludedUserDB
+import com.core.data.remote.AppGateway
 import com.core.data.remote.entity.LoginEntity
 import com.core.data.remote.entity.UserEntity
 import com.core.data.repository.mapper.UserEntityDataMapper
+import com.core.domain.User
 import io.reactivex.Single
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-import org.mockito.ArgumentMatchers
-import org.mockito.Mockito
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnit
+import org.mockito.junit.MockitoRule
 
-class UserRepositoryTest : BaseRepositoryTest() {
+class UserRepositoryTest : BaseTest() {
+
+    @get:Rule
+    val mockitoRule: MockitoRule = MockitoJUnit.rule()
+
+    @Mock
+    lateinit var gateway: AppGateway
+    @Mock
+    lateinit var excludedUserDao: ExcludedUserDao
 
     var userEntityList = listOf(
         UserEntity(login = LoginEntity("1")),
@@ -20,23 +36,33 @@ class UserRepositoryTest : BaseRepositoryTest() {
         UserEntity(login = LoginEntity("5"))
     )
 
+    var excludedUserList = listOf(
+        ExcludedUserDB("1"),
+        ExcludedUserDB("2")
+    )
+
+    var result = listOf(
+        User("3"),
+        User("5")
+    )
+
     lateinit var userRepository: UserRepositoryImpl
 
     @Before
     override fun setUp() {
         super.setUp()
 
-        userRepository = UserRepositoryImpl(gateway, UserEntityDataMapper())
+        userRepository = UserRepositoryImpl(gateway, excludedUserDao, UserEntityDataMapper())
     }
 
     @Test
-    fun `Get users from server and remove duplicates`() {
-        Mockito.`when`(gateway.users(ArgumentMatchers.anyInt(), ArgumentMatchers.anyInt()))
-            .thenReturn(Single.just(userEntityList))
+    fun `Get users from server and check that duplicates and excluded users were removed`() {
+        `when`(gateway.users(anyInt(), anyInt())).thenReturn(Single.just(userEntityList))
+        `when`(excludedUserDao.findAll()).thenReturn(Single.just(excludedUserList))
 
         userRepository.list(1, 100)
             .test()
             .assertNoErrors()
-            .assertValue { it.size() == 4 }
+            .assertValues(result)
     }
 }
