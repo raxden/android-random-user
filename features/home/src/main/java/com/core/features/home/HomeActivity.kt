625335bd7a9c43d562b7357d678a8634ada6a4e7
Ángel Gómez <raxden.dev@gmail.com>
2019-10-30 17:37:03 +0100

    add toolbar lifecycle to activity

--- features/home/src/main/java/com/core/features/home/HomeActivity.kt
+++ features/home/src/main/java/com/core/features/home/HomeActivity.kt
@@ -1,14 +1,17 @@
 package com.core.features.home
 
 import android.view.View
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.ViewModelProvider
 import com.core.activity.BaseFragmentActivity
 import com.core.common.android.extensions.getExtras
 import com.core.features.home.databinding.HomeActivityBinding
 import com.core.lifecycle.activity.InjectFragmentActivityLifecycle
+import com.core.lifecycle.activity.ToolbarActivityLifecycle
 import javax.inject.Inject
 
 class HomeActivity : BaseFragmentActivity<HomeActivityBinding>(),
+    ToolbarActivityLifecycle.Callback,
     InjectFragmentActivityLifecycle.Callback<HomeFragment> {
 
     @Inject
@@ -23,13 +26,18 @@ class HomeActivity : BaseFragmentActivity<HomeActivityBinding>(),
 
     override fun onBindingCreated(binding: HomeActivityBinding) {
         binding.setVariable(BR.viewModel, viewModel)
-        binding.executePendingBindings()
     }
 
     override fun finish() {
         super.finish()
         overridePendingTransition(0, 0)
     }
+
+    // =============== ToolbarActivityLifecycle ====================================================
+
+    override fun onCreateToolbarView(): Toolbar = binding.toolbarView
+
+    override fun onToolbarViewCreated(toolbar: Toolbar) {}
 
     // =============== InjectFragmentActivityLifecycle =============================================
 
