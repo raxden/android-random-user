0ecb33ec987458797dcfaf634c7c2a47a7482236
Ángel Gómez <raxden.dev@gmail.com>
2019-10-30 17:37:15 +0100

    add DiffUtil to UserModel

--- features/home/src/main/java/com/core/features/home/model/UserModel.kt
+++ features/home/src/main/java/com/core/features/home/model/UserModel.kt
@@ -1,8 +1,9 @@
 package com.core.features.home.model
 
+import androidx.recyclerview.widget.DiffUtil
 import com.core.domain.User
 
-class UserModel(
+data class UserModel(
     val id: String,
     val name: String,
     val email: String,
@@ -17,4 +18,17 @@ class UserModel(
         user.picture.thumbnail,
         user.phone
     )
+
+    companion object {
+        var DIFF_CALLBACK = object : DiffUtil.ItemCallback<UserModel>() {
+            override fun areItemsTheSame(oldItem: UserModel, newItem: UserModel): Boolean {
+                return oldItem.id === newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: UserModel, newItem: UserModel): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
 }
+
