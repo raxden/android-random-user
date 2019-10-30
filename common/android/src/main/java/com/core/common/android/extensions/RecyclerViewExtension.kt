68621a594493303609cd2935e22604a2c3d31e07
Ángel Gómez <raxden.dev@gmail.com>
2019-10-30 17:32:36 +0100

    add Recyclerview extension to detect when page endless

---
+++ common/android/src/main/java/com/core/common/android/extensions/RecyclerViewExtension.kt
@@ -0,0 +1,29 @@
+package com.core.common.android.extensions
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import timber.log.Timber
+
+interface OnPageEndlessListener {
+    fun onPageEndless()
+}
+
+fun RecyclerView.setOnPageEndlessListener(listener: OnPageEndlessListener) {
+    addOnScrollListener(object : RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            if (pageEndlessDetect(recyclerView))
+                listener.onPageEndless()
+        }
+    })
+}
+
+private fun pageEndlessDetect(recyclerView: RecyclerView): Boolean {
+    return (recyclerView.layoutManager as? LinearLayoutManager)?.let {
+        val visibleItemCount = it.childCount
+        val totalItemCount = it.itemCount
+        val pastVisibleItems = it.findFirstVisibleItemPosition()
+        val endless = pastVisibleItems + visibleItemCount >= totalItemCount - visibleItemCount
+        Timber.d("PAGER: $endless = ($pastVisibleItems + $visibleItemCount >= $totalItemCount - $visibleItemCount)")
+        endless
+    } ?: false
+}
