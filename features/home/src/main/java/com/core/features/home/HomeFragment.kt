7c2881d86c08c514e7be9ff35f9e26cac6cd0e4b
Ángel <raxden.dev@gmail.com>
2019-11-01 18:29:02 +0100

    handle possible error when retrieve results from usecase

--- features/home/src/main/java/com/core/features/home/HomeFragment.kt
+++ features/home/src/main/java/com/core/features/home/HomeFragment.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.core.common.android.Status
 import com.core.common.android.extensions.OnPageEndlessListener
 import com.core.common.android.extensions.setOnPageEndlessListener
 import com.core.features.home.adapter.HomeListAdapter
 import com.core.features.home.databinding.HomeFragmentBinding
 import com.core.features.home.model.UserModel
 import com.core.fragment.BaseViewFragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -55,8 +58,21 @@ class HomeFragment : BaseViewFragment<HomeFragmentBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel.throwable.observe(viewLifecycleOwner, Observer {
+            MaterialAlertDialogBuilder(activity)
+                .setTitle(R.string.home_error_unknown_title)
+                .setMessage(R.string.home_error_unknown_message)
+                .setPositiveButton(android.R.string.ok) { dialog, _ -> dialog.dismiss() }
+                .create()
+                .show()
+        })
         viewModel.users.observe(viewLifecycleOwner, Observer {
-            homeListAdapter.submitList(it.data?.toList())
+            when (it.status) {
+                Status.ERROR -> Toast
+                    .makeText(context, R.string.home_error_to_retrieve_users, Toast.LENGTH_LONG)
+                    .show()
+                else -> homeListAdapter.submitList(it.data?.toList())
+            }
         })
     }
 
