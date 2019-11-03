3f316f04cd6109333b5a3c7e15f66b0e392f3d37
Ángel <raxden.dev@gmail.com>
2019-11-03 10:40:08 +0100

    remove unnecesary module

--- app/src/main/java/com/core/app/di/module/FeaturesModule.kt
+++ app/src/main/java/com/core/app/di/module/FeaturesModule.kt
@@ -2,12 +2,10 @@ package com.core.app.di.module
 
 import com.core.features.detail.di.DetailModule
 import com.core.features.home.di.HomeModule
-import com.core.features.splash.di.SplashModule
 import dagger.Module
 
 @Module(
     includes = [
-        SplashModule::class,
         HomeModule::class,
         DetailModule::class
     ]
